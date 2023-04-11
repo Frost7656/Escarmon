4b8bbff79467939308891779a1278ab1de160a0c
Noah ABRAHAM <noahabraham214@gmail.com>
2023-04-11 16:53:09 +0200

    First commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+
+</script>
+<nav class="bg-slate-200">
+    <ul class="flex">
+        <li class="mr-6">Accueil</li>
+        <li>Contenu</li>
+        <li>Entrainement</li>
+        <li>Reproduction</li>
+        <li>Collection</li>
+    </ul>
+    <div>
+        <div>
+            <p>141</p>
+        </div>
+        <div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+</nav>
